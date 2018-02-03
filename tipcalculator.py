ab14ae4f0f14e2a0e1df8477ef824e6bd25c8dc6
imsamahos <sohamasmi@gmail.com>
2018-02-03 01:21:28 -0800

    Added gst calculation

--- tipcalculator.py
+++ tipcalculator.py
@@ -2,12 +2,16 @@ billAmount=0
 tipRate=0
 tip=0
 total=0
+gstRate=0.05
+gstAmount=0
 
 billAmount = int(raw_input('Enter the bill amount: '))
 tipRate = int(raw_input('Enter the tip rate: '))
 
 tip = billAmount * tipRate/100
 total =  billAmount + tip
+gstAmount = int(total * gstRate)
 
 print('The tip is', tip)
 print('The total is', total)
+print('Gst rate is {} Gst amount is {}'.format(gstRate, gstAmount))
